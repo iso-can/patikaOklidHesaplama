8801beaf975dad87b13b8eaeb5eaee34ae63dbef
ismail <iso-can@users.noreply.github.com>
2025-01-05 03:05:50 +0300

    Update oklidHesaplama.py

--- oklidHesaplama.py
+++ oklidHesaplama.py
@@ -3,6 +3,7 @@ def euclideanDistance(x,y):
     distances = 0
     for i in range(len(x)):
         distances += (x[i] - y[i]) * (x[i] - y[i])
+        distances **= 0.5
         return distances
 point1 = [1, 1]
 point2 = [5, 5]
